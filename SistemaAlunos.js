a313db4e5f9f13af51cc225873f205673a3cd2b2
andrepadua34 <andrepadua34@gmail.com>
2021-12-09 19:29:44 -0300

    Aula Remota 2 - For / While / For Of

---
+++ SistemaAlunos.js
@@ -0,0 +1,37 @@
+let nomedealuno =["Marcel", "André", "Rita", "Afranio","Isabelle","Isadora","Iasmin","Gabriel"]
+
+for (let nome of nomedealuno) {
+    console.log(`Esta pessoa se chama ${nome}`)    
+}
+
+
+
+
+// let numerodealunos = 10;
+
+// let contador = 0;
+
+// while(contador <= numerodealunos) {
+//     //console.log(contador) 
+    
+//     if(contador == 0){
+//         console.log("O numero atual é zero")
+//         }else if(contador % 2 != 0){
+//             console.log("O número " + contador + " é Ímpar")
+//         }else{
+//             console.log(`O número ${contador} é Par`)
+//         }
+//         contador++;
+//     }
+
+// for (let contador = 0; contador <= numerodealunos; contador++) {
+// //console.log(contador) 
+
+// if(contador == 0){
+//     console.log("O numero atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("O número " + contador + " é Par")
+//     }else{
+//         console.log(`O número ${contador} é Ímpar`)
+//     }
+// }
